952b78a5d26c269e0861aee4c59cfc54356c8c63
Vicky <101943576+april808@users.noreply.github.com>
2023-10-08 21:34:49 +0800

    基礎設定用，改打包名稱

--- vue-test/vue.config.js
+++ vue-test/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  filenameHashing: false /* 關閉打包後的檔名雜湊值 */,
+  outputDir: "dist" /* 打包生成的文件目錄，預設是 dist */,
+  pages: {
+    index: {
+      entry: "src/main.js",
+      filename:
+        "test.html" /* 自定義的文件名，輸出在文件目錄，預設是 index.html */,
+    },
+  },
+});
