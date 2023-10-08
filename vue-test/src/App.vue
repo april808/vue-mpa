0a2c9036e0dcf7f225592474d51980bc387bdac5
Vicky <101943576+april808@users.noreply.github.com>
2023-10-09 00:36:18 +0800

    jQuery 引用方式

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -2,9 +2,28 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <hr>
+    <img src="https://fakeimg.pl/300x200/200">
   </nav>
   <router-view/>
 </template>
+
+<script>
+import $ from 'jquery';
+
+
+export default {
+  name: 'app',
+  components: {
+
+  },
+  mounted() {
+    // 在 mounted 階段使用 jQuery ($) 確保 DOM 已經渲染完成
+    console.log($('img').attr('src'));
+  },
+}
+
+</script>
 
 <style lang="scss">
 #app {
--- vue-test/vue.config.js
+++ vue-test/vue.config.js
@@ -1,5 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" /* 設置打包的路徑 */ ? "./" : "/",
   transpileDependencies: true,
   filenameHashing: false /* 關閉打包後的檔名雜湊值 */,
   outputDir: "dist" /* 打包生成的文件目錄，預設是 dist */,
@@ -7,7 +9,12 @@ module.exports = defineConfig({
     index: {
       entry: "src/main.js",
       filename:
-        "test.html" /* 自定義的文件名，輸出在文件目錄，預設是 index.html */,
+        "index.html" /* 自定義的文件名，輸出在文件目錄，預設是 index.html */,
+    },
+  },
+  configureWebpack: {
+    externals: {/* 防止 webpack 打包進專案中 */
+      jquery: "$",
     },
   },
 });
