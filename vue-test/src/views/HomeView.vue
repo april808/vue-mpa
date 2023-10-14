10780b3e9181f6d5bf0f1422f4890b8461d3f913
Vicky <101943576+april808@users.noreply.github.com>
2023-10-14 23:58:22 +0800

    pinia實作

--- vue-test/src/views/HomeView.vue
+++ vue-test/src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <h2>{{ num }}</h2>
+
+    {{ sampleStore }}
+    <hr />
+    sampleStore.name::
+    <p>
+      {{ sampleStore.name }}
+    </p>
+    <hr />
+    {{ sampleStore.changeName(2565) }}
+    <hr />
+    {{ sampleStore.name }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { ref } from "vue";
+import { useSampleStore } from "@/stores/sampleStore.js";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  setup() {
+    const num = ref(1); // Assuming num is reactive
+    const sampleStore = useSampleStore();
+
+    return {
+      sampleStore,
+      num,
+    };
+  },
+};
 </script>
