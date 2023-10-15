cce5d5e2fdd3e08e681e1881274b4be3d5de3c39
Vicky <101943576+april808@users.noreply.github.com>
2023-10-15 20:01:18 +0800

    移除vuex

--- vue-test/src/main.js
+++ vue-test/src/main.js
@@ -2,12 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
-app.use(store);
 app.use(router);
 app.mount("#app");
